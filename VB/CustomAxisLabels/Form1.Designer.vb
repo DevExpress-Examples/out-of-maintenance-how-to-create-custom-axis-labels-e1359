e6a9076da262f1d922570958eae0e9c69e9e8211
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomAxisLabels/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿Namespace CustomAxisLabels
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
+            Dim series1 As New DevExpress.XtraCharts.Series()
+            Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("Illinois", New Object() { (CObj(423.721R))})
+            Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("Indiana", New Object() { (CObj(178.719R))})
+            Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("Michigan", New Object() { (CObj(308.845R))})
+            Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("Ohio", New Object() { (CObj(348.555R))})
+            Dim seriesPoint5 As New DevExpress.XtraCharts.SeriesPoint("Wisconsin", New Object() { (CObj(160.274R))})
+            Dim series2 As New DevExpress.XtraCharts.Series()
+            Dim seriesPoint6 As New DevExpress.XtraCharts.SeriesPoint("Illinois", New Object() { (CObj(476.851R))})
+            Dim seriesPoint7 As New DevExpress.XtraCharts.SeriesPoint("Indiana", New Object() { (CObj(195.769R))})
+            Dim seriesPoint8 As New DevExpress.XtraCharts.SeriesPoint("Michigan", New Object() { (CObj(335.793R))})
+            Dim seriesPoint9 As New DevExpress.XtraCharts.SeriesPoint("Ohio", New Object() { (CObj(374.771R))})
+            Dim seriesPoint10 As New DevExpress.XtraCharts.SeriesPoint("Wisconsin", New Object() { (CObj(182.373R))})
+            Dim series3 As New DevExpress.XtraCharts.Series()
+            Dim seriesPoint11 As New DevExpress.XtraCharts.SeriesPoint("Illinois", New Object() { (CObj(528.904R))})
+            Dim seriesPoint12 As New DevExpress.XtraCharts.SeriesPoint("Indiana", New Object() { (CObj(227.271R))})
+            Dim seriesPoint13 As New DevExpress.XtraCharts.SeriesPoint("Michigan", New Object() { (CObj(372.576R))})
+            Dim seriesPoint14 As New DevExpress.XtraCharts.SeriesPoint("Ohio", New Object() { (CObj(418.258R))})
+            Dim seriesPoint15 As New DevExpress.XtraCharts.SeriesPoint("Wisconsin", New Object() { (CObj(211.727R))})
+            Dim chartTitle1 As New DevExpress.XtraCharts.ChartTitle()
+            Me.chartControl = New DevExpress.XtraCharts.ChartControl()
+            Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
+            CType(Me.chartControl, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(series1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(series2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(series3, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl
+            ' 
+            Me.chartControl.DataBindings = Nothing
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisY.WholeRange.AlwaysShowZeroLevel = False
+            Me.chartControl.Diagram = xyDiagram1
+            Me.chartControl.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Right
+            Me.chartControl.Legend.Direction = DevExpress.XtraCharts.LegendDirection.LeftToRight
+            Me.chartControl.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl.Name = "chartControl"
+            series1.Name = "1998"
+            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4, seriesPoint5})
+            series2.Name = "2001"
+            series2.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint6, seriesPoint7, seriesPoint8, seriesPoint9, seriesPoint10})
+            series3.Name = "2004"
+            series3.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint11, seriesPoint12, seriesPoint13, seriesPoint14, seriesPoint15})
+            Me.chartControl.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1, series2, series3}
+            Me.chartControl.Size = New System.Drawing.Size(384, 216)
+            Me.chartControl.TabIndex = 0
+            chartTitle1.Font = New System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+            chartTitle1.Text = "Great Lakes Gross State Product"
+            Me.chartControl.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() { chartTitle1})
+            ' 
+            ' defaultLookAndFeel1
+            ' 
+            Me.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2013"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(384, 216)
+            Me.Controls.Add(Me.chartControl)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(series1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(series2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(series3, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.chartControl, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private chartControl As DevExpress.XtraCharts.ChartControl
+        Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
+    End Class
+End Namespace
+
